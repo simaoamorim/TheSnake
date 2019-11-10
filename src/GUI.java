4985e4810c3d717c550a277ec4171f44b4421110
simaoamorim <simao_amorim@outlook.pt>
2019-11-10 13:37:43 +0100

    GUI fully initialized

--- src/GUI.java
+++ src/GUI.java
@@ -1,7 +1,74 @@
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 
 class GUI extends JFrame {
+    private Grid grid;
+    private JSlider slider;
+    private JPanel panel2;
+    private static final Dimension frameSize = new Dimension(600,600);
+    private static final Dimension maxFrameSize = Toolkit.getDefaultToolkit().getScreenSize();
+
     GUI() {
-        // Gui initialization here
+        setSize(frameSize);
+        setMinimumSize(new Dimension(200, 200));
+        setMaximumSize(maxFrameSize);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null); // Center the frame on the screen
+        initUI();
+        pack();
+        setVisible(true);
     }
+
+    private void initUI() {
+        setLayout(new BorderLayout());
+        JPanel buttonsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JButton button = new JButton("Reset");
+        button.setActionCommand("reset");
+        button.addActionListener(this::buttonActions);
+        slider = new JSlider(JSlider.HORIZONTAL);
+        slider.setMinimum(1);
+        slider.setMaximum(50);
+        slider.setValue(10);
+        slider.addChangeListener(this::zoomChosen);
+        button.setSize(100,60);
+        buttonsPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        buttonsPanel.add(button);
+        buttonsPanel.add(new JSeparator(JSeparator.HORIZONTAL));
+        buttonsPanel.add(new JLabel("Zoom:"));
+        buttonsPanel.add(slider);
+        add(buttonsPanel, BorderLayout.SOUTH);
+        grid = new Grid();
+        panel2 = new JPanel();
+        panel2.setLayout(new FlowLayout(FlowLayout.CENTER));
+        panel2.add(grid);
+        JScrollPane mainPanel = new JScrollPane(panel2);
+        add(mainPanel);
+    }
+
+    private void buttonActions(ActionEvent e) {
+        String command = e.getActionCommand();
+        switch (command) {
+            case "reset" : {
+                slider.setValue(10);
+                pack(); // Resize the window to fit contents
+                setLocationRelativeTo(null); // Move window to the center of the screen
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+    }
+
+    private void zoomChosen(ChangeEvent e) {
+        int reqSize = slider.getValue();
+        System.out.println(String.format("Setting zoom to %d", reqSize));
+        grid.setCellSize(reqSize);
+        grid.repaint();
+        grid.revalidate();
+        panel2.setPreferredSize(new Dimension((int)grid.getPreferredSize().getWidth()+5, (int)grid.getPreferredSize().getHeight()+5));
+    }
+
 }
---
+++ src/Grid.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+class Grid extends JComponent {
+    private java.util.List<Point> activeCells;
+    private int cellSize = 10;
+    private static final double margin = 1.0;
+    private double XCount = 50.0;
+    private double YCount = 50.0;
+
+    Grid() {
+        activeCells = new ArrayList<>();
+        this.setPreferredSize(new Dimension((int)((XCount *cellSize)+(2*margin)), (int)((YCount *cellSize)+(2*margin))));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        double cellWidth = cellSize;
+        double cellHeight = cellSize;
+        System.out.println(String.format("Cell size: %dx%d", (int)cellWidth, (int)cellHeight));
+        for (Point fillCell : activeCells) {
+            int cellX = (int)(margin + (fillCell.x * cellWidth));
+            int cellY = (int)(margin + (fillCell.y * cellHeight));
+            g.setColor(Color.BLACK);
+            g.fillRect(cellX, cellY, (int)cellWidth, (int)cellHeight);
+        }
+        double _width = (XCount *cellWidth);
+        double _height = (YCount *cellHeight);
+        g.drawRect( (int) margin, (int) margin, (int) _width, (int)_height);
+        for (double x = margin; x <= _width; x += cellWidth) {
+            g.drawLine((int) x, (int) margin, (int) x, (int) (_height+margin));
+        }
+        for (double y = margin; y <= _height; y += cellHeight) {
+            g.drawLine((int) margin, (int) y, (int) (_width+margin), (int) y);
+        }
+    }
+
+    void setCellSize(int size) {
+        cellSize = size;
+        this.setPreferredSize(new Dimension((int)((XCount *cellSize)+(2*margin)), (int)((YCount *cellSize)+(2*margin))));
+    }
+
+    void setXCount(int count) {XCount = count;}
+
+    void setYCount(int count) {YCount = count;}
+
+    void setActiveCells(List<Point> list) {activeCells.clear(); activeCells.addAll(list);}
+
+    void appendActiveCell(Point point) {activeCells.add(point);}
+}
